c6cd24579f67f32bb71ff4379a3b90f219de5873
drewrobertsmith <drew.robert.smith@gmail.com>
2024-01-20 11:45:09 -0600

    Added Expo Router

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function Homepage() {
+  return (
+    <SafeAreaView>
+        <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
